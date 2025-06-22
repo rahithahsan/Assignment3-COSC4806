244879f38f86e39d9e41357bbf07eb8175ef9f40
rahithahsan <rahsan@algomau.ca>
2025-06-22 07:24:31 +0000

    style(secret): tidy markup and show friendly date on secret page

--- app/views/secret/index.php
+++ app/views/secret/index.php
@@ -1,12 +1,7 @@
-<?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are at a secret page</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
-            </div>
-        </div>
-    </div>
+<?php require_once 'app/views/templates/header.php'; ?>
+<div class="container mt-4">
+  <h1>You reached the secret page!</h1>
+  <p class="lead"><?= date('F jS, Y'); ?></p>
+</div>
+<?php require_once 'app/views/templates/footer.php'; ?>
 
-    <?php require_once 'app/views/templates/footer.php' ?>
